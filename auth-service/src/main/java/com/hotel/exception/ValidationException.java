e91c8cd7b0b99b91cd63272a030e50b41e8382e7
AlexandroRey06 <alex.rey@uac.edu.co>
2024-11-26 00:57:24 -0500

    datele back

--- auth-service/src/main/java/com/hotel/exception/ValidationException.java
+++
@@ -1,7 +0,0 @@
-package com.hotel.exception;
-
-public class ValidationException extends RuntimeException {
-    public ValidationException(String message) {
-        super(message);
-    }
-}
--- auth-service/src/main/java/com/hotel/factory/DatabaseType.java
+++
@@ -1,6 +0,0 @@
-package com.hotel.factory;
-
-public enum DatabaseType {
-    PRIMARY_DB,
-    SECONDARY_DB
-}
